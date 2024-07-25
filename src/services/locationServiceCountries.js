6d244e10611a4142ae742a2655be60a50ab2ee71
SeredenkoDS <w.skybird@gmail.com>
2024-07-26 00:41:51 +0300

    rewrote 'catch' part according to feedback

--- src/services/locationServiceCountries.js
+++ src/services/locationServiceCountries.js
@@ -1,3 +1,6 @@
+const { createError } = require('~/utils/errorsHelper')
+const { COUNTRYSTATECITY_API_ISSUE } = require('~/consts/errors')
+
 const locationServiceCountries = {
   getCountries: async (api_key) => {
     var headers = new Headers()
@@ -14,7 +17,7 @@ const locationServiceCountries = {
       const result = await response.text()
       return JSON.parse(result)
     } catch (error) {
-      //
+      throw createError(401, COUNTRYSTATECITY_API_ISSUE)
     }
   }
 }
