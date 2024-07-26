4dd1e874819ba538b2e5013c253ee9011033f6f7
SeredenkoDS <w.skybird@gmail.com>
2024-07-26 15:49:09 +0300

    quickfix var -> const

--- src/services/locationServiceCountries.js
+++ src/services/locationServiceCountries.js
@@ -3,10 +3,10 @@ const { COUNTRYSTATECITY_API_ISSUE } = require('~/consts/errors')
 
 const locationServiceCountries = {
   getCountries: async (api_key) => {
-    var headers = new Headers()
+    const headers = new Headers()
     headers.append('X-CSCAPI-KEY', api_key)
 
-    var requestOptions = {
+    const requestOptions = {
       method: 'GET',
       headers: headers,
       redirect: 'follow'
