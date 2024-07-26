afd52f41410231bf7ecd9f1b24cd0c59265526eb
sachkova_yuliia <sachkova_yuliia@gmail.com>
2024-07-26 09:32:36 +0200

    removed all unnnecessary lines of code

--- src/services/email.js
+++ src/services/email.js
@@ -19,27 +19,20 @@ const emailTemplates = new EmailTemplates({
 })
 const emailService = {
   sendEmail: async (email, subject, language, text = {}) => {
-    console.log('Sending email to:', email) // test
-    console.log('Email subject:', subject) // test
-    console.log('Email text:', text) // test
     const templateToSend = templateList[subject]
 
     if (!templateToSend) {
-      console.error(`Template for subject ${subject} not found`) // test
       throw createError(404, TEMPLATE_NOT_FOUND)
     }
 
     const langTemplate = templateToSend[language]
 
     if (!langTemplate) {
-      console.error(`Template for language ${language} not found`) // test
       throw createError(404, TEMPLATE_NOT_FOUND)
     }
 
     try {
-      console.log(`Rendering template ${langTemplate.template} with text`, text) // test
       const html = await emailTemplates.render(langTemplate.template, text)
-      console.log('HTML content generated') // test
 
       await sendMail({
         from: `Space2Study <${user}>`,
@@ -47,9 +40,7 @@ const emailService = {
         subject: langTemplate.subject,
         html
       })
-      console.log(`Email sent successfully to ${email}`) // test
     } catch (error) {
-      console.error('Error sending email:', error) // test
       throw createError(500, 'Failed to send email')
     }
   }
