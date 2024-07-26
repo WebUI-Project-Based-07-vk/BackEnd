82f9a31601351fd8d86bd91eb4d03e1043187f56
SeredenkoDS <w.skybird@gmail.com>
2024-07-26 20:31:24 +0300

    Update src/services/locationServiceCities.js
    response.text() => response.json()
    
    Co-authored-by: vlad-khrychov <67462207+vlad-khrychov@users.noreply.github.com>

--- src/services/locationServiceCities.js
+++ src/services/locationServiceCities.js
@@ -14,8 +14,8 @@ const locationServiceCities = {
 
     try {
       const response = await fetch(`https://api.countrystatecity.in/v1/countries/${iso2_code}/cities`, requestOptions)
-      const result = await response.text()
-      return JSON.parse(result)
+      const result = await response.json()
+      return result 
     } catch (error) {
       throw createError(401, COUNTRYSTATECITY_API_ISSUE)
     }
